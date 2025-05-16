9e0a1ecf7db8e5b44e760bcf31b7ea01548c453e
EmilioM1105 <emiliomarquez110504@gmail.com>
2025-05-16 17:05:55 -0600

    Segundo ejercicio (función de flecha)
    Código del segundo ejercicio

--- Practica1/Variables.js
+++ Practica1/Variables.js
@@ -6,3 +6,9 @@ nombre = "Brayan Alejandro";
 let saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 console.log(saludo);
 
+//Ejercicio b
+const cuadrado = (numero) => {
+    return numero * numero;
+}
+
+console.log(cuadrado(9))
