f85b49204fc82f664670d4ecfe8acd782f93025d
EmilioM1105 <emiliomarquez110504@gmail.com>
2025-05-20 12:51:18 -0600

    Practica 2-ejercicio c
    Uso de métodos .find, .forEach y .reduce

--- Practica2/ObjetosyArreglos.js
+++ Practica2/ObjetosyArreglos.js
@@ -25,4 +25,21 @@ const nombres = productos
     .filter(producto => producto.precio > 1000)
     .map(producto => producto.nombre);
 
-console.log(nombres); 
+console.log(nombres);
+
+//Ejercicio c
+const personas = [
+    { nombre: "Ana", edad: 22 },
+    { nombre: "Luis", edad: 35 },
+    { nombre: "María", edad: 28 }
+];
+
+const luis = personas.find(persona => persona.nombre === "Luis");
+console.log("Persona encontrada:", luis);
+
+personas.forEach(persona => {
+    console.log(`${persona.nombre} tiene ${persona.edad} años`);
+});
+
+const totalEdades = personas.reduce((acum, persona) => acum + persona.edad, 0);
+console.log("Suma total de edades:", totalEdades);
